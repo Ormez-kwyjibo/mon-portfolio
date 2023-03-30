3d3aa27d8d0f7853b402c53676e5f05aebf61f5a
Ormez-kwyjibo <127950397+Ormez-kwyjibo@users.noreply.github.com>
2023-03-30 14:01:08 +0200

    dinesh portfolio

---
+++ js/script.js
@@ -0,0 +1,6 @@
+/* =RESIZE NAV BAR ON SCROLL= */
+var navbar=document.querySelector(".navbar");
+/* when the scroll is higher than 20 viewport height, add the sticky class to the tag with a class navbar*/
+window.onscroll = ()=>{
+    this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+}
